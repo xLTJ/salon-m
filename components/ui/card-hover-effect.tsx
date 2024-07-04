a3381cb59f6ff5e5d5e6bb12b6049e5ebc276627
xLTJ <lxcy.3@proton.me>
2024-07-05 01:54:41 +0200

    checking if this is actually whats fucking mobile up

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -40,23 +40,23 @@ export const HoverEffect = ({
                     onMouseEnter={() => setHoveredIndex(idx)}
                     onMouseLeave={() => setHoveredIndex(null)}
                 >
-                    {!isPhone && <AnimatePresence>
-                        {hoveredIndex === idx && (
-                            <motion.span
-                                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] rounded-3xl"
-                                layoutId="hoverBackground"
-                                initial={{opacity: 0}}
-                                animate={{
-                                    opacity: 1,
-                                    transition: {duration: 0.15},
-                                }}
-                                exit={{
-                                    opacity: 0,
-                                    transition: {duration: 0.15, delay: 0.2},
-                                }}
-                            />
-                        )}
-                    </AnimatePresence>}
+                    {/*{!isPhone && <AnimatePresence>*/}
+                    {/*    {hoveredIndex === idx && (*/}
+                    {/*        <motion.span*/}
+                    {/*            className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] rounded-3xl"*/}
+                    {/*            layoutId="hoverBackground"*/}
+                    {/*            initial={{opacity: 0}}*/}
+                    {/*            animate={{*/}
+                    {/*                opacity: 1,*/}
+                    {/*                transition: {duration: 0.15},*/}
+                    {/*            }}*/}
+                    {/*            exit={{*/}
+                    {/*                opacity: 0,*/}
+                    {/*                transition: {duration: 0.15, delay: 0.2},*/}
+                    {/*            }}*/}
+                    {/*        />*/}
+                    {/*    )}*/}
+                    {/*</AnimatePresence>}*/}
                     <Card image_url={item.image_url}>
                         <CardTitle>{item.title}</CardTitle>
                         <CardDescription>{item.description}</CardDescription>
