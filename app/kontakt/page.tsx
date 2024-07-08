345a03a38a0255350df71b343041ae0df28f0988
xLTJ <lxcy.3@proton.me>
2024-07-08 19:35:26 +0200

    fixed

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,6 +1,5 @@
 import ContactHero from "@/components/kontakt-page/contact-hero";
 import ContactInfoSection from "@/components/kontakt-page/contact-info-section";
-import {LayoutGridDemo} from "@/components/gallery/image-gallery";
 
 export default function Page() {
     return (
